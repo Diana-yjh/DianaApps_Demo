51f453bf1f8c2fbc1d545030faf8a2cdd9e884a6
Diana-yjh <projin1234@gmail.com>
2024-06-03 14:27:33 +0900

    docs: DianaMap_Demo 추가

---
+++ DianaMap_Demo/DianaMap_Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DianaMap_Demo
+//
+//  Created by Yejin Hong on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DianaMap_Demo/DianaMap_Demo/DianaMap_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DianaMap_DemoApp.swift
+//  DianaMap_Demo
+//
+//  Created by Yejin Hong on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct DianaMap_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
